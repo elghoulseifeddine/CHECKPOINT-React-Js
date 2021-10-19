b01bb67a28f90a225e66f2296a6b4cde14bb346b
Abderrahim Trabelsi <trabelsi.abderrahim@petrogas.local>
2021-10-19 16:21:12 +0100

    Checkpoint ReactJs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Forme from './Forme/Forme.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Forme />
+    </div>
+  );
+}
+
+export default App;
